8d62651762cdf95035b0adf2caca5edc540dc398
saminoorsyed <saminoorsyed@gmail.com>
2023-10-17 21:42:25 -0400

    finish exercise 3.4

--- week2/programmingProject/exercise_3.4/README.md
+++ week2/programmingProject/exercise_3.4/README.md
@@ -1,2 +1,19 @@
+## Exercise 3.4 directions
+
+1) unzip the repository into it's own directory
+2) that directory should have three files:
+    a) brute_force.py
+    b) words.txt
+    c) README.md
+3) cd into the directory with the files and run the following command:
+
+python3 brute_force.py 'This is a top secret.' '8d20e5056a8d24d0462ce74e4904c1b513e10d1df4a2ef2ad4540fae1ca0aaf9'
+
+Notes: 
+1) the first argument is the plaintext, the second is the ciphertext. 
+2) The relative location of words.txt is hard coded into the program, so it is essential that it be in the same directory that you are in when you run the program
+3) this script uses the cryptography package that comes with python and makes use of openssl.
+
+
 Sources:
 https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/
--- week2/programmingProject/exercise_3.4/brute_force.py
+++ week2/programmingProject/exercise_3.4/brute_force.py
@@ -1,43 +1,41 @@
-# what do we know:
-# aes-128-cbc
-# IV = 000000...
-# key = english word shorter than 16 characters with spaces at the end to pad
-
-
 import argparse
-from cryptography import Cipher, algorithms, modes
-
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding as pad
 
 def main():
-    parser = argparse.ArgumentParser(description="finds the cryptographic key based on the words.txt, plain.txt and cipher.txt")
-    parser.add_argument("plaintext", help= "provide the plaintext as a string arg", type= str)
-    parser.add_argument("ciphertext", help="provide the cipher hex string as a string argument", type=str)
+    parser = argparse.ArgumentParser(description="finds the cryptographic key based on the words.txt, plain.txt, and cipher.txt")
+    parser.add_argument("plaintext", help="provide the plaintext as a hexadecimal string", type=str)
+    parser.add_argument("ciphertext", help="provide the cipher hex string as a hexadecimal string", type=str)
 
     args = parser.parse_args()
-    plaintext = args.plaintext
-    ciphertext = args.ciphertext
+    plaintext = args.plaintext.encode('utf-8')  # Convert hex string to bytes
+    ciphertext = bytes.fromhex(args.ciphertext)  # Convert hex string to bytes
 
     if not plaintext or not ciphertext:
         print("missing either plaintext or ciphertext, please provide them")
         return
-    
-    with open('words.txt')as file:
+
+    with open('words.txt') as file:
         word_list = [line.strip() for line in file]
-    
-    # define IV outside of for loop as a 16 bytes of 0
-    iv = b'\x00'*16
 
+    # Define IV outside of for loop as 16 bytes of 0
+    iv = b'\x00' * 16
+    print(plaintext[:21])
     for word in word_list:
-        padding = ' '*(16-len(word))
-        key = word+padding
-        # default is aes 128
-        cipher = Cipher(algorithms.AES(key), modes.CBC(iv))
+        if len(word) >= 16:
+            continue
+        padding_str = ' ' * (16 - len(word))
+        key = (word + padding_str)
 
-        # decrypt the cipher text and match it with the plaintext
+        cipher = Cipher(algorithms.AES(key.encode('utf-8')), modes.CBC(iv))
+
+        # Decrypt the ciphertext and match it with the plaintext
         decryptor = cipher.decryptor()
-        decryption = decryptor.update(ciphertext)
-
-        if decryption == plaintext:
+        decryption = decryptor.update(ciphertext) + decryptor.finalize()
+        if decryption[:21] == plaintext:
             print(f"key: {word}")
             print(f"decrypted text: {decryption}")
-            print(f"plaintext     : {plaintext}")
+            print(f"plaintext     : {plaintext}")  # Convert bytes back to hex for printing
+
+if __name__ == "__main__":
+    main()
